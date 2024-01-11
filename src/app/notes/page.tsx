87b5bc78c1c9a509d35c28c5df8a0e682ad836de
Mike Kistler <mikekistler@microsoft.com>
2024-01-10 19:35:53 -0600

    Add page metadata

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FlowBrain - notes",
+};
+
 export default function NotesPage() {
-  return <div>Here will be your notes</div>
+  return <div>Here will be your notes</div>;
 }
